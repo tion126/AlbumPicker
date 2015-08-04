e142d4e9ae3ebd1a327411db5d33fbd91261d910
LiSiYuan <lsy0812@qq.com>
2015-08-04 18:04:28 +0800

    add Swift

---
+++ AlbumPicker/AlbumPicker/LSYAlbumPicker_Swift/ViewController/LSYAlbumCatalog.swift
@@ -0,0 +1,14 @@
+//
+//  LSYAlbumCatalog.swift
+//  AlbumPicker
+//
+//  Created by okwei on 15/8/4.
+//  Copyright (c) 2015年 okwei. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LSYAlbumCatalog:UIViewController {
+    
+}
